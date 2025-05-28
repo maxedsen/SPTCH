ee3256146e7efe5aec5fca220db933849f165812
maxedsen <maxedsen@yandex.com>
2025-05-29 01:47:47 +0500

    Add files via upload

---
+++ ScriptForReview.py
@@ -0,0 +1,19 @@
+﻿import requests
+
+def requestCheck():
+    url = "https://search.yandex-team.ru/suggest/?text=Саптех&version=2&people.per_page=10"
+    response = requests.get(url)
+    data = response.json()
+    return parseChecker(data)
+
+def parseChecker(response: dict) -> list[str]:
+    logins = []
+    if "people" in response and "per_page" in response["people"]:
+        people = response["people"].get("items", [])
+        for person in people:
+            if "login" in person:
+                logins.append(person["login"])
+    return logins
+if __name__ == "__main__":
+    logins = requestCheck()
+    print("Логины:", logins)
